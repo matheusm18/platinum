fc87d34dab97256f737cdbbceaee3b7129760032
Matheus <50931752+matheusm18@users.noreply.github.com>
2026-04-25 12:19:35 +0100

    feat: setup db with Docker, PostgreSQL and Drizzle (#3)

---
+++ src/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+
+const globalForDb = globalThis as unknown as { client: postgres.Sql };
+
+const client = globalForDb.client ?? postgres(process.env.DATABASE_URL!);
+
+if (process.env.NODE_ENV !== "production") globalForDb.client = client;
+
+export const db = drizzle(client, { schema });
